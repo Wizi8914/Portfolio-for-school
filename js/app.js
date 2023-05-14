5badb21dcd45aef12685e2d4aa2680743fe31b8e
Wizi8914 <loick.konieczny@gmail.com>
2023-05-14 23:13:01 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        var monElement = document.querySelector('.next-page-button');
+        monElement.style.display = 'block';
+    }, 3800);
+});
+
+function nextPage() {
+    var monElement = document.querySelector('.first-container');
+    monElement.className = 'first-container-fadeOut';
+    setTimeout(function() {
+        monElement.style.display = 'none';
+    }, 2000);
+}
